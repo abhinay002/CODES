f496da49b0e533db3499e6a71d7dd864799e379d
abhinay002 <102654759+abhinay002@users.noreply.github.com>
2022-05-27 15:47:55 +0530

    Add files via upload

---
+++ ROLLINGDICE.py
@@ -0,0 +1,14 @@
+import random
+
+rollDice = ""
+rollDice = input("press enter to play")
+roll_again =""
+while roll_again =="":
+    print("player1",random.randint(1,6))
+    print("player2",random.randint(1, 6))
+    print(input(""))
+
+
+
+
+
---
+++ instabiqr.py
@@ -0,0 +1,10 @@
+import qrcode
+img =qrcode.make("https://instagram.com/abi._.1.23?utm_medium=copy_link")
+
+img.save("instaabi.png")
+img.show()
+
+
+
+
+
